10545889203e941f7fafa55c5d6f16379c092604
Dinesh <dinesh.melapundi@gmail.com>
2023-07-31 17:27:10 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo   
+from flask_cors import CORS
+import os
+from bson.objectid import ObjectId
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+
+mongo = PyMongo(app)
+CORS(app)
+
+@app.route('/todos', methods=['GET'])
+def get_all_todos():
+    todos = mongo.db.todos
+    result = []
+    for field in todos.find():
+        result.append({'_id': str(field['_id']), 'todo': field['todo']})
+    return jsonify(result)
+
+
+@app.route('/todo', methods=['POST'])
+def add_todo():
+    todos = mongo.db.todos
+    todo = request.json['todo']
+    # result = todos.insert_one({'todo': todo}) 
+    result = todos.insert_one({'todo': todo, 'completed': False}) 
+    todo_id = result.inserted_id  
+    new_todo = todos.find_one({'_id': todo_id })
+    return jsonify({'_id': str(new_todo['_id']), 'todo': new_todo['todo']})
+
+
+@app.route('/todo/<id>', methods=['PUT'])
+def update_todo(id):
+    print("Updating todo with id: ", id)
+    todos = mongo.db.todos
+    todo = request.json['todo']
+    # todos.update_one({'_id': ObjectId(id)}, {"$set": {'todo': todo}}, upsert=False)
+    todos.update_one({'_id': ObjectId(id)}, {"$set": {'todo': todo, 'completed': False}}, upsert=False)
+    updated_todo = todos.find_one({'_id': ObjectId(id)})
+    return jsonify({'_id': str(updated_todo['_id']), 'todo': updated_todo['todo']})
+
+@app.route('/todo/<id>/complete', methods=['PUT'])
+def complete_todo(id):
+    todos = mongo.db.todos
+    completed = request.json['completed']
+    todos.update_one({'_id': ObjectId(id)}, {"$set": {'completed': completed}}, upsert=False)
+    completed_todo = todos.find_one({'_id': ObjectId(id)})
+
+    # Add check for None
+    if completed_todo is None:
+        return jsonify({"error": "No todo found with the given id"}), 404
+
+    return jsonify({'_id': str(completed_todo['_id']), 'todo': completed_todo['todo'], 'completed': completed_todo['completed']})
+
+
+@app.route('/todo/<id>', methods=['DELETE'])
+def delete_todo(id):
+    print("Deleting todo with id: ", id)
+    todos = mongo.db.todos
+    result = todos.delete_one({'_id': ObjectId(id)})
+    
+    if result.deleted_count == 1:
+        response = {'status': 'success'}
+    else:
+        response = {'status': 'error'}
+    return jsonify(response)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)), debug=True)
