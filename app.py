d2670d70c0e2781154ed29f0077416cdc9d635af
Dinesh <dinesh.melapundi@gmail.com>
2023-07-31 18:25:21 +0530

    mongo url specified

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo   
 from flask_cors import CORS
-import os
+# import os
 from bson.objectid import ObjectId
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+app.config["MONGO_URI"] = "mongodb+srv://mldinesh0:DPGq3M5xFkYGKfD6@cluster0.mnugi39.mongodb.net/mydatabase?retryWrites=true&w=majority&tlsAllowInvalidCertificates=true"
 
 mongo = PyMongo(app)
 CORS(app)
