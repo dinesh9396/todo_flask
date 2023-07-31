c8afb9a3c1d293acb81b1d289c2f74ad0f3ae070
Dinesh <dinesh.melapundi@gmail.com>
2023-07-31 18:41:13 +0530

    guincorn added

--- app.py
+++ app.py
@@ -70,4 +70,5 @@ def delete_todo(id):
     return jsonify(response)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)), debug=True)
+    # app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)), debug=True)
+    app.run(debug=True)
